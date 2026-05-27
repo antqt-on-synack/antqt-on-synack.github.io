943d2b80b663cc7f71e5fbb0aa72faa0b6bb2a88
antqt-on-synack <antqt.on.synack@gmail.com>
2026-05-27 07:03:03 +0000

    Remove getfiles.sh and paths.txt scripts to clean up unused files

--- cc/getfiles.sh
+++
@@ -1,5 +0,0 @@
-cat paths.txt | while read p; do
-  clean=$(python3 -c "import os; print(os.path.normpath('$p').lstrip('/'))")
-  mkdir -p "$(dirname "$clean")"
-  curl "https://cms-admin-uat.concung.com$p" -o "$clean" -x "http://192.168.12.142:8081" -k
-done
